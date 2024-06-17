2cf3dbd76585e848580503e1a9a892a4816d68dc
Rogu3Ag3nt <kpeel1@myjmc.edu.au>
2024-06-17 14:34:21 +1000

    initial upload
    moving project around for tracking

---
+++ AGA307-Assessment-1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController Controller;
+
+    public float speed = 12f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        Controller.Move(move * speed * Time.deltaTime); 
+
+    }
+}
